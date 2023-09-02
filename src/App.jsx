33cbe90139e79f7516c0efe071c0139b47924f4c
nazmulhossain17 <nazmulhossain17sn@gmail.com>
2023-09-02 19:39:52 +0600

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,30 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage/HomePage";
 import LoginPage from "./Pages/LoginPage/LoginPage";
 import ProfilePage from "./Pages/ProfilePage/ProfilePage";
+import { ThemeProvider } from "@emotion/react";
+import { useSelector } from "react-redux";
+import { useMemo } from "react";
+import { CssBaseline, createTheme } from "@mui/material";
+import { themeSettings } from "./theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  // const isAuth = Boolean(useSelector((state) => state.token));
+
   return (
-    <>
-      <div className="app">
-        <BrowserRouter>
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
           <Routes>
             <Route path="/" element={<LoginPage />} />
             <Route path="/home" element={<HomePage />} />
             <Route path="/profile/:userId" element={<ProfilePage />} />
           </Routes>
-        </BrowserRouter>
-      </div>
-    </>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
   );
 }
 
